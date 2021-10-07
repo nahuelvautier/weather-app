f79383f93d7710d99b39018f1839d22752073957
Mr. Robot <nahuelearning@gmail.com>
2021-10-07 02:24:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { FormGetter } from "./components/FormGetter";
+
+function App() {
+  return (
+    <>
+      <header className="header-container">
+        <nav>
+          <h1>Climapp</h1>
+          <FormGetter />
+        </nav>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FormGetter.jsx
@@ -0,0 +1,10 @@
+export const FormGetter = () => {
+  return (
+    <div>
+      <form>
+        <input type="search" />
+        <input type="submit" value="Buscar" />
+      </form>
+    </div>
+  )
+}
