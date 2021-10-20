2d1059f51cbf605dc054a1faebed704ac9eeea17
Mr. Robot <nahuelearning@gmail.com>
2021-10-20 15:16:50 -0300

    background mod

--- src/components/CurrentInfo.jsx
+++ src/components/CurrentInfo.jsx
@@ -12,7 +12,8 @@ const CurrentInfo = ({ current, weather, main, wind, sys, kelvinToCelcius }) => 
   if (iconUrl.includes("d@4x")) {
     //document.getElementById("root").style.backgroundImage = "url(https://i.pinimg.com/originals/bc/22/a8/bc22a864b99bfbfab6c1e7b52973916a.gif)";
     document.getElementById("root").style.backgroundImage = "url(https://img.allw.mn/content/sn/x3/wi29hyxi54ef412c818ca644447713_640x360.gif)";
-    document.getElementById("root").style.backgroundSize = "cover";
+    document.getElementById("root").style.backgroundSize = "100% 100%";
+    //document.getElementById("root").style.backgroundSize = "cover";
     document.getElementById("root").style.backgroundRepeat = "no-repeat";
     document.getElementById("root").style.backgroundAttachment = "fixed";
   } else if (iconUrl.includes("n@4x")) {
