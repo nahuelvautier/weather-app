728f8e476a2aaecf8ad811b7ac02e402c6be65be
Mr. Robot <nahuelearning@gmail.com>
2021-10-22 00:45:37 -0300

    Weather App v1.1

--- src/components/CurrentInfo.jsx
+++ src/components/CurrentInfo.jsx
@@ -10,8 +10,6 @@ const CurrentInfo = ({ current, weather, main, wind, sys, kelvinToCelcius }) => 
   //console.log(sys);
 
   if (iconUrl.includes("d@4x")) {
-    //document.getElementById("root").style.backgroundImage = "url(https://i.pinimg.com/originals/bc/22/a8/bc22a864b99bfbfab6c1e7b52973916a.gif)";
-    //document.getElementById("root").style.backgroundImage = "url(https://img.allw.mn/content/sn/x3/wi29hyxi54ef412c818ca644447713_640x360.gif)";
     document.getElementById("root").style.backgroundImage = "url(./assets/light-sky.gif)";
     document.getElementById("root").style.backgroundSize = "100% 100%";
     document.getElementById("root").style.backgroundRepeat = "no-repeat";
@@ -19,7 +17,6 @@ const CurrentInfo = ({ current, weather, main, wind, sys, kelvinToCelcius }) => 
     document.getElementById("root").style.backgroundAttachment = "fixed";
     document.getElementById("root").style.backgroundColor = "#AAA";
   } else if (iconUrl.includes("n@4x")) {
-    //document.getElementById("root").style.backgroundImage = "url(https://i.gifer.com/origin/c5/c5660392f5ffd5b4632dd944f3de2134.gif)";
     document.getElementById("root").style.backgroundImage = "url(./assets/night-sky.gif)";
     document.getElementById("root").style.backgroundSize = "100% 100%";
     document.getElementById("root").style.backgroundRepeat = "no-repeat";
@@ -40,16 +37,16 @@ const CurrentInfo = ({ current, weather, main, wind, sys, kelvinToCelcius }) => 
         <figcaption>{weather[0].description}</figcaption>
       </figure>
       <aside className="current-weather-specs">
-        <div>
-          <div><p>Temperatura: {kelvinToCelcius(main.temp)}°C</p></div>
-          <div><p>Mínima: {kelvinToCelcius(main.temp_min)}°C</p></div>
-          <div><p>Máxima: {kelvinToCelcius(main.temp_max)}°C</p></div>
-        </div>
-        <div>
-          <div><p>Humedad: {main.humidity}%</p></div>
-          <div><p>S. Térm:{kelvinToCelcius(main.feels_like)}°C</p></div>
-          <div><p>Viento: {wind.speed} km/h</p></div>
-        </div>
+        <span>
+          <p>Temperatura: {kelvinToCelcius(main.temp)}°C</p>
+          <p>Mínima: {kelvinToCelcius(main.temp_min)}°C</p>
+          <p>Máxima: {kelvinToCelcius(main.temp_max)}°C</p>
+        </span>
+        <span>
+          <p>Humedad: {main.humidity}%</p>
+          <p>S. Térm:{kelvinToCelcius(main.feels_like)}°C</p>
+          <p>Viento: {wind.speed} km/h</p>
+        </span>
       </aside>
     </article>
   );
--- src/components/FooterContent.jsx
+++ src/components/FooterContent.jsx
@@ -1,10 +1,18 @@
 const FooterContent = () => {
+  let styles = {
+    marginBottom: ".25rem",
+    padding: "1rem",
+    borderTop: "thin solid #fff",
+  }
+
   return (
     <>
-      <aside style={{ marginBottom: ".5rem" }} >
+      <aside style={styles} >
         <p>Datos obtenidos de <a href="https://openweathermap.org/" target="_blank" rel="noreferrer">openweathermap.org</a></p>
       </aside>
-      <p>&copy; 2021 <a href="https://nahuelvautier.netlify.app" target="_blank" rel="noreferrer">Nahuel Vautier</a></p>
+      <div style={{ transform: "translate(-1.5rem, 0)" }}>
+        <p>&copy; 2021 <a href="https://nahuelvautier.netlify.app" target="_blank" rel="noreferrer">Nahuel Vautier</a></p>
+      </div>
     </>
   )
 }
